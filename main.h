057af0625a0c94cc0da03402eca10bca1b5506cf
monsiisaiah1212 <monsiisaiah2525@outlook.com>
2022-10-17 10:56:37 -0700

    Header file

--- main.h
+++ main.h
@@ -42,5 +42,5 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-		va_list list, char buffer[], int flags, int width, int precision, int size);
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
