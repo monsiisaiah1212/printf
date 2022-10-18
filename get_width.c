bc7db9877e6b4993b6da7505950fd181ad19c670
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-18 14:20:50 -0700

    add

--- get_width.c
+++ get_width.c
@@ -12,7 +12,7 @@ int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
 	int width = 0;
-
+	
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -31,6 +31,6 @@ int get_width(const char *format, int *i, va_list list)
 	}
 
 	*i = curr_i - 1;
-
+	
 	return (width);
 }
